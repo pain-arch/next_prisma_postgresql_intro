5352327c9afae0c89a39c0371356006368bf07bf
pain-arch <himulol123@gmail.com>
2024-04-02 16:41:57 +0600

    error issue

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,9 +1,11 @@
 export default function Post ({id, title, content, authorName}){
     return(
+        <div> All the Post are Here
         <article style={{border: '2px solid black', padding: '15px', margin: '20px 0px' }}>
             <h2>{authorName}</h2>
             <h4>{title}</h4>
             <p>{content}</p>
         </article>
+        </div>
     )
 }
